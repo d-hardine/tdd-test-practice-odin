3d764dedfbadecee379de25ab8b70d0b7e06ceee
Dhiva Ryan H <176643369+d-hardine@users.noreply.github.com>
2025-02-24 17:57:28 +0700

    add more tests

--- capitalize/capitalize.test.js
+++ capitalize/capitalize.test.js
@@ -1,5 +1,9 @@
 const capitalize = require('./capitalize')
 
-test('takes a string and returns it with the first character capitalized', () => {
-    expect(capitalize('jancok')).toBe('Jancok')
+test('takes a string ("hello") and returns it with the first character capitalized ("Hello")', () => {
+    expect(capitalize('hello')).toBe('Hello')
+})
+
+test('takes a string ("world") and returns it with the first character capitalized ("World")', () => {
+    expect(capitalize('world')).toBe('World')
 })
--- reverse-string/reverse-string.test.js
+++ reverse-string/reverse-string.test.js
@@ -1,5 +1,9 @@
 const reverseStr = require('./reverse-string')
 
-test('takes a string and returns it with the first character capitalized', () => {
-    expect(reverseStr('jancok')).toBe('kocnaj')
+test('takes a string ("hello") and returns it returns it reversed ("olleh")', () => {
+    expect(reverseStr('hello')).toBe('olleh')
+})
+
+test('takes a string ("world") and returns it returns it reversed ("dlrow")', () => {
+    expect(reverseStr('world')).toBe('dlrow')
 })
